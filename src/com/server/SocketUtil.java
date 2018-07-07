b3fc42996b6448abd64d4434d3f70e29b57d7a69
Anil Verma <anil.verma@e2open.com>
2018-07-07 16:41:20 +0530

    Debug your queries and form params using this server

---
+++ src/com/server/SocketUtil.java
@@ -0,0 +1,24 @@
+package com.server;
+
+import java.net.*;
+import java.io.*;
+
+public class SocketUtil {
+	/** Make a BufferedReader to get incoming data. */
+
+	public static BufferedReader getReader(Socket s)
+			throws IOException {
+		return(new BufferedReader(
+				new InputStreamReader(s.getInputStream())));
+	}
+	/** Make a PrintWriter to send outgoing data.
+	 * This PrintWriter will automatically flush stream
+	 * when println is called.
+	 */
+	public static PrintWriter getWriter(Socket s)
+			throws IOException {
+		// Second argument of true means autoflush.
+		return(new PrintWriter(s.getOutputStream(), true));
+	}
+
+}
